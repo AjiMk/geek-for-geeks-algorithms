846ae38e860c047c1657a0ece0c9f983c6f7d0ab
Ajay Kumar M <ajay.kumar@phases.dk>
2022-08-29 07:35:17 +0530

    prime check issue fixed

--- primality-check/solution.py
+++ primality-check/solution.py
@@ -2,7 +2,9 @@ import math
 
 class Solution:
     def isPrime(self,N):
-        if N <= 3:
+        if N==1:
+            return False
+        if N==2 or N==3:
             return True
 
         for i in range(2, math.floor(N**0.5)+1):
