1fcf665f8e434f25fc281b8054ccf1d9c80893a0
Ajay Kumar M <ajay.kumar@phases.dk>
2022-10-26 21:24:54 +0530

    problem: palindrome of a number -updated.

--- practice/recurssion/palindrome/palindrome-of-a-number.py
+++ practice/recurssion/palindrome/palindrome-of-a-number.py
@@ -1,3 +1,6 @@
+"""
+    https://www.geeksforgeeks.org/check-if-a-number-is-palindrome/
+"""
 def palindromeOfANumber(c_number, number):
     if number//10==0:
         return number%10==c_number[0]%10
@@ -7,3 +10,10 @@ def palindromeOfANumber(c_number, number):
     c_number[0] = c_number[0]//10
 
     return (number % 10 == c_number[0] % 10)
+
+number = 112211
+c_number = [number]
+if palindromeOfANumber(c_number, number):
+    print("palindrome")
+else:
+    print("not palindrome")
